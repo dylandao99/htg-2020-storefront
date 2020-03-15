314f3fada6f6dd73cbbe660f32c23425455732b6
Dylan <dylandao99@gmail.com>
2020-03-14 21:08:11 -0400

    map

--- src/views/Home/Page.tsx
+++ src/views/Home/Page.tsx
@@ -1,7 +1,9 @@
 import "./scss/index.scss";
 
 import classNames from "classnames";
+import HEREMap from 'here-maps-react';
 import * as React from "react";
+import HPlatform, { HMap, HMapMarker, HMapPolyLine} from "react-here-map";
 import { Link } from "react-router-dom";
 
 import { Button, Loader, ProductsFeatured } from "../../components";
@@ -17,6 +19,26 @@ import { structuredData } from "../../core/SEO/Homepage/structuredData";
 
 import noPhotoImg from "../../images/no-photo.svg";
 
+const points = [
+  { lat: 52.5309825, lng: 13.3845921 },
+  { lat: 52.5311923, lng: 13.3853495 },
+  { lat: 52.5313532, lng: 13.3861756 },
+  { lat: 52.5315142, lng: 13.3872163 },
+  { lat: 52.5316215, lng: 13.3885574 },
+  { lat: 52.5320399, lng: 13.3925807 },
+  { lat: 52.5321472, lng: 13.3935785 },
+];
+
+const coords = { lat: 52.5309825, lng: 13.3845921 };
+
+const icon =
+  '<svg width="24" height="24" ' +
+  'xmlns="http://www.w3.org/2000/svg">' +
+  '<rect stroke="white" fill="#1b468d" x="1" y="1" width="22" ' +
+  'height="22" /><text x="12" y="18" font-size="12pt" ' +
+  'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+  'fill="white">H</text></svg>';
+
 const Page: React.FC<{
   loading: boolean;
   categories: ProductsList_categories;
@@ -29,9 +51,37 @@ const Page: React.FC<{
 
   return (
     <>
+  <HPlatform
+    app_id="2Ts3vDUTLPW8kNUtyFRY"
+    app_code="MDivMVFtNkpim-dWuetlWw"
+    useCIT
+    useHTTPS
+	interactive
+    includeUI
+    includePlaces
+  >
+    <HMap
+      style={{
+        height: "400px",
+        width: "800px",
+      }}
+mapOptions={{ center: { lat: 52.5321472, lng: 13.3935785 } }}
+    useEvents // Required for events
+// tslint:disable-next-line:no-console
+    mapEvents={{ pointerdown: e => console.log("Map Pointer Down", e) }} // event handlers
+    >
+      <HMapPolyLine points={points} />
+<HMapMarker coords={coords} icon={icon} />
+    </HMap>
+  </HPlatform>,
+  document.getElementById("app")
       <script className="structured-data-list" type="application/ld+json">
         {structuredData(shop)}
       </script>
+	<HEREMap
+	      appId="s1kDjVfOnj3pb1K9WQ3D"
+	      appCode="_rUjaYJvp7j5NUDV2UHXfb23i_sa0eOekP_74XuarQo"
+	    />
       <div
         className="home-page__hero"
         style={
